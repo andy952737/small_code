8c213ae292207fa894ac87605b80ec6b0f9a9fd9
Andy <andy952737@gmail.com>
2020-06-13 19:39:00 +0800

    callback

--- _ruby/model.rb
+++ _ruby/model.rb
@@ -1,5 +1,17 @@
 class Model < ActiveRecord::Base
 
-	# Model design ...  
+	 # callback 
+	 # https://rails.ruby.tw/active_record_callbacks.html
+	 validates :login, :email, presence: true
+ 
+	 before_validation :ensure_login_has_a_value
+	 
+	 protected
+
+     def ensure_login_has_a_value
+       if login.nil?
+         self.login = email unless email.blank?
+       end
+     end
 
 end
