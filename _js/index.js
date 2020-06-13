68ea109a641c508bafd9ef2be7d1c0f51836d35e
Andy <andy952737@gmail.com>
2020-06-13 19:27:31 +0800

    監聽 元素

--- _js/index.js
+++ _js/index.js
@@ -1,1 +1,8 @@
-var demo;
+// JavaScript 如何使用addEventListener() 監聽HTML元素的事件 
+window.onload = function() {
+    var input1 = document.getElementById("input1");
+    
+    input1.addEventListener("keydown", function (event) {
+        alert(this.value);
+    });
+}; 
